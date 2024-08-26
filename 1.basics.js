43f03ea53832839bdcd134aaf7f7717fd9371e25
Madn <madan.patakota@gmail.com>
2024-08-26 20:36:41 +0530

    javascript

---
+++ 1.basics.js
@@ -0,0 +1,112 @@
+//window.alert("Hello world");
+
+//   window.alert(1+2+3);
+
+//   console.log("Hello world");
+
+//   console.log(1+2+3);
+
+
+// Real time world .. Data is having the diff types
+
+    // 1 // number
+
+    // "javascript" // string
+    // "javascript123"    // string
+    // "%^%$$$RRRRR"
+    // "John smith !@#@@@"   //string
+
+    // "j"  //charcter
+
+//Javascript --->  variable
+
+//case study 
+
+ console.log("Javascript");
+
+
+ //if you want to capture the data for doing some activities you need the some reference.
+ 
+ var courseName = "javasacript";
+
+ var customerID = 1;
+
+
+ //window.alert(courseName);
+
+ /*
+ ------------------------
+ --------------------------
+ -------------------------
+ */
+
+
+ console.log(courseName);
+ console.log(customerID);
+
+
+
+
+ /* 
+
+  Q) what is the datatype ?
+  A : The way of you are showing the data is nothing but datatype
+
+Generally data would be different formats 
+ Example 1 :  1 ,       999.99 , "Y" ,   "Javascript"
+              number , decimal , char , string 
+ So the way you are representing the data
+
+ Q) So how to use it this data in JS ?
+ A : with the help of the variable.
+
+*/
+
+
+//var courseName = "javasacript";
+
+var empName; // declaration the variable
+    empName = "Robert"; //Initlization the variable
+
+// var empName = "Robert";
+console.log(empName);
+
+//one kind of strategy
+
+// camel case letters
+
+
+
+var courseName ="Javascript";
+//var customerID = "1"
+var customerId = 1;
+
+var customerLocation = "London";
+
+var fullSentence = "The way of you are showing the data is nothing but datatype";
+
+
+
+var employeeId = 1;         //or var customer_id = 1;
+var employeeSalary = 999.99 //var employee_salary = 999.99
+var customerLocation = "Hyderabad";
+var properCamelCase  = "sample";
+var proper_camel_case  = "sample";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
---
+++ index.js
@@ -0,0 +1,16 @@
+// function fnname(){
+//       ----------------------------//
+
+// }
+
+//function showMessage() {
+  // i want to show message in browser window
+  // window.alert("Hello world");
+
+  //   I want to show message in console window
+//   console.log("Hello world");
+// }
+
+// time to understand on event
+
+// event is nothing but a action.
